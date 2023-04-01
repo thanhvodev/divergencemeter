a3052ee50f515de6d1ea82bc00e3ec7bba96e8bf
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2023-04-01 20:27:36 +0700

    update cos to second

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import divergenceValueMap from "./divergenceValue";
 import "./App.css";
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { utf8ToBytes } from "ethereum-cryptography/utils";
+import { useState } from "react";
 
 function App() {
-  const message = utf8ToBytes(new Date().getFullYear().toString());
-  const hash = keccak256(message);
+  const [dvValue, setDvValue] = useState("");
 
-  let divergenceKey = hash[0] % 11;
-  let divergenceValue = divergenceValueMap[divergenceKey];
+  setInterval(() => {
+    const message = utf8ToBytes(new Date().getSeconds().toString());
+    const hash = keccak256(message);
+
+    let divergenceKey = hash[0] % 11;
+    let divergenceValue = divergenceValueMap[divergenceKey];
+    setDvValue(divergenceValue);
+  }, 1000);
 
   return (
     <div
       className="body"
       style={{
-        backgroundImage: `url(${divergenceValue})`,
+        backgroundImage: `url(${dvValue})`,
         backgroundPosition: "center",
       }}
     ></div>
