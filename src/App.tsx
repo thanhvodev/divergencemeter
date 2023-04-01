e94848a4dedbf9bbe01685a780040eee9a4fd8d6
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2023-04-01 21:07:22 +0700

    change after every seconds

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,37 @@ import divergenceValueMap from "./divergenceValue";
 import "./App.css";
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { utf8ToBytes } from "ethereum-cryptography/utils";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [dvValue, setDvValue] = useState("");
 
-  setInterval(() => {
-    const message = utf8ToBytes(new Date().getSeconds().toString());
-    const hash = keccak256(message);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const message = utf8ToBytes(new Date().getSeconds().toString());
+      const hash = keccak256(message);
+      let divergenceKey = hash[0] % 11;
+      let divergenceValue = divergenceValueMap[divergenceKey];
+      setDvValue(divergenceValue);
+      console.log(divergenceValue);
+    }, 1000);
 
-    let divergenceKey = hash[0] % 11;
-    let divergenceValue = divergenceValueMap[divergenceKey];
-    setDvValue(divergenceValue);
-  }, 1000);
+    return () => clearInterval(intervalId); // Cleanup the interval when component is unmounted
+  }, []);
 
   return (
     <div
-      className="body"
       style={{
-        backgroundImage: `url(${dvValue})`,
-        backgroundPosition: "center",
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-    ></div>
+    >
+      <img src={dvValue} alt="divergence value" />
+    </div>
   );
 }
 
